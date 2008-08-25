a8c971f3ea4acdb01a3688a902b5981cb1d9f4cc
ylixir <ylixir@9e574f74-bb53-0410-98d3-8d7354be1c59>
2008-08-25 20:04:56 +0000

    menu module completed. form is all we have left now.
    git-svn-id: https://ycurses.googlecode.com/svn/trunk@20 9e574f74-bb53-0410-98d3-8d7354be1c59

--- modules/menu.d
+++ modules/menu.d
@@ -145,7 +145,7 @@ void menu_format(MENU* menu, int* rows, int* cols);
 Set/get the hook to be called when a menu posts and when the item
 selection changes (after the change).
 
-See_also: menu_hook
+See_also: man menu_hook
 */
 int set_item_init(MENU* menu, void* function(MENU*) func);
 ///ditto
@@ -154,7 +154,7 @@ void* function(MENU*) item_init(MENU* menu);
 Set/get the hook to be called when a menu unposts and when the item
 selection changes (before the change).
 
-See_also: menu_hook
+See_also: man menu_hook
 */
 int set_item_term(MENU* menu, void * function(MENU*) func);
 ///ditto
@@ -163,7 +163,7 @@ void* function(MENU*) item_term(MENU* menu);
 Set/get the hook to be called when a menu posts and after the top row on the
 menu changes.
 
-See_also: menu_hook
+See_also: man menu_hook
 */
 int set_menu_init(MENU* menu, void* function(MENU*) func);
 ///ditto
@@ -172,48 +172,204 @@ void* function(MENU*) menu_init(MENU* menu);
 Set/get the hook to be called when a menu unposts and before the top row on
 the menu changes.
 
-See_also: menu_hook
+See_also: man menu_hook
 */
 int set_menu_term(MENU* menu, void* function(MENU*) func);
 ///ditto
 void* function(MENU*) menu_term(MENU* menu);
 
-ITEM* new_item(char* name, char* description);
-int free_item(ITEM* item);
-MENU* new_menu(ITEM** items);
-int free_menu(MENU* menu);
-int post_menu(MENU* menu);
-int unpost_menu(MENU* menu);
-int set_menu_opts(MENU* menu, OPTIONS opts);
-int menu_opts_on(MENU* menu, OPTIONS opts);
-int menu_opts_off(MENU* menu, OPTIONS opts);
-OPTIONS menu_opts(MENU* menu);
-int set_menu_win(MENU* menu, WINDOW* win);
-WINDOW* menu_win(MENU* menu);
-int set_menu_sub(MENU* menu, WINDOW* sub);
-WINDOW* menu_sub(MENU* menu);
-int scale_menu(MENU* menu, int* rows, int* columns);
-int set_menu_mark(MENU* menu, char* mark);
-char* menu_mark(MENU* menu);
+/**
+Set/get array of menu items associated with the menu.
+Array is null terminated.
+
+See_also: man menu_items
+*/
 int set_menu_items(MENU* menu, ITEM** items);
+///ditto
 ITEM **menu_items(MENU* menu);
+/**
+Get the number of items in the menu.
+
+See_also: man menu_items
+*/
 int item_count(MENU* menu);
-int set_item_value(ITEM* item, bool value);
-bool item_value(ITEM* item);
-char *item_name(ITEM* item);
-char *item_description(ITEM* item);
-int set_item_opts(ITEM* item, OPTIONS opts);
-int item_opts_on(ITEM* item, OPTIONS opts);
-int item_opts_off(ITEM* item, OPTIONS opts);
-OPTIONS item_opts(ITEM* item);
+
+/**
+Set/get the string used to mark the selected menu items.
+
+See_also: man menu_mark
+*/
+int set_menu_mark(MENU* menu, char* mark);
+///ditto
+char* menu_mark(MENU* menu);
+
+/**
+Allocate/deallocate a new menu.
+
+See_also: man menu_new
+*/
+MENU* new_menu(ITEM** items);
+///ditto
+int free_menu(MENU* menu);
+
+/**
+Sets/gets menu option bits.
+
+See_also: man menu_opts
+*/
+int set_menu_opts(MENU* menu, OPTIONS opts);
+///ditto
+int menu_opts_on(MENU* menu, OPTIONS opts);
+///ditto
+int menu_opts_off(MENU* menu, OPTIONS opts);
+///ditto
+OPTIONS menu_opts(MENU* menu);
+
+
+/**
+Set/get the pattern match buffer for the menu.
+
+See_also: man menu_pattern
+*/
+int set_menu_pattern(MENU* menu, char* pattern);
+///ditto
+char* menu_pattern(MENU* menu);
+
+/**
+Display/erase a menu.
+
+See_also: man man menu_post
+*/
+int post_menu(MENU* menu);
+///ditto
+int unpost_menu(MENU* menu);
+
+/**
+Gets printable name of a request code, or visa versa
+
+See_also: man menu_requestname
+*/
+char* menu_request_name(int request);
+///ditto
+int menu_request_by_name(char* name);
+
+/**
+Set/retrieve the spacing information for the menu.
+
+See_also: man menu_spacing
+*/
+int set_menu_spacing(MENU* menu, int spc_description,
+    int spc_rows, int spc_columns);
+///ditto
+int menu_spacing(MENU* menu, int* spc_description,
+    int* spc_rows, int* spc_columns);
+
+/**
+Set/retrieve the user pointer for the menu
+
+See_also: man menu_userptr
+*/
+int set_menu_userptr(MENU* menu, void* userptr);
+///ditto
+void* menu_userptr(MENU* menu);
+
+/**
+Get/set the windows associated with the menus.
+
+See_also: man menu_win
+*/
+int set_menu_win(MENU* menu, WINDOW* win);
+///ditto
+WINDOW* menu_win(MENU* menu);
+///ditto
+int set_menu_sub(MENU* menu, WINDOW* sub);
+///ditto
+WINDOW* menu_sub(MENU* menu);
+/**
+Get the minimum required size of window for the menu.
+
+See_also: man menu_win
+*/
+int scale_menu(MENU* menu, int* rows, int* columns);
+
+/**
+Get/set the current menu item.
+
+See_also: man mitem_current
+*/
 int set_current_item(MENU* menu, ITEM* item);
+///ditto
 ITEM* current_item(MENU* menu);
+/**
+Set the item that is the top row of the menu.
+
+See_also: man mitem_current
+*/
 int set_top_row(MENU* menu, int row);
+///ditto
 int top_row(MENU* menu);
+/**
+Get the index of the item in the menu.
+
+See_also: man mitem_current
+*/
 int item_index(ITEM* item);
+
+/**
+Get the name and description of the item.
+
+See_also: man mitem_name
+*/
+char *item_name(ITEM* item);
+///ditto
+char *item_description(ITEM* item);
+
+/**
+Allocate/deallocate a menu item.
+
+See_also: man mitem_new
+*/
+ITEM* new_item(char* name, char* description);
+///ditto
+int free_item(ITEM* item);
+
+/**
+Set/get an items option bits
+
+See_also: man mitem_opts
+*/
+int set_item_opts(ITEM* item, OPTIONS opts);
+///ditto
+int item_opts_on(ITEM* item, OPTIONS opts);
+///ditto
+int item_opts_off(ITEM* item, OPTIONS opts);
+///ditto
+OPTIONS item_opts(ITEM* item);
+
+/**
+Get set the user pointer associated with the item.
+
+See_also: man mitem_userptr
+*/
 int set_item_userptr(ITEM* item, void* userptr);
+///ditto
 void* item_userptr(ITEM* item);
 
+/**
+Get set whether an item in a multivalued menu is selected or not.
+
+See_also: man mitem_value
+*/
+int set_item_value(ITEM* item, bool value);
+///ditto
+bool item_value(ITEM* item);
+
+/**
+See whether the item is visible or not.
+
+See_also: man mitem_visible
+*/
+bool item_visible(ITEM* item);
 
 enum :OPTIONS
 {

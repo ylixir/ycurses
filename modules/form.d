590a8f82d2fa97c4c5055a0689176e2888b26636
ylixir <ylixir@9e574f74-bb53-0410-98d3-8d7354be1c59>
2008-09-01 01:44:36 +0000

    added the man_pages.txt file, to help track progress, this file won't be around long
    git-svn-id: https://ycurses.googlecode.com/svn/trunk@21 9e574f74-bb53-0410-98d3-8d7354be1c59

--- modules/form.d
+++ modules/form.d
@@ -1,6 +1,7 @@
-/*
-Copyright (c) 2008 Jon "ylixir" Allen <ylixir@gmail.com>
-
+/**
+ * Authors: Jon "ylixir" Allen, ylixir@gmail.com
+ * Copyright: Copyright (c) 2008 ylixir. All rights reserved.
+ * License:
     Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the "Software"),
 to deal in the Software without restriction, including without limitation
@@ -19,7 +20,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 IN THE SOFTWARE.
 */
-
 module form;
 
 version(Tango)
@@ -31,7 +31,7 @@ else
   import std.c.stdarg;
 }
 
-public import ncurses;
+public import ncurses, eti;
 
 extern(C):
 
@@ -47,6 +47,154 @@ extern FIELDTYPE* TYPE_NUMERIC;
 extern FIELDTYPE* TYPE_REGEXP;
 extern FIELDTYPE* TYPE_IPV4;
 
+/**
+Restores the cursor to the position required by the forms driver.
+
+See_also: man form_cursor
+*/
+int pos_form_cursor(FORM* form);
+
+/**
+Check and see if there is offscreen data ahead/behind in the form.
+
+See_also: man form_data
+*/
+bool data_ahead(FORM* form);
+///ditto
+bool data_behind(FORM* form);
+
+/**
+Routine to funnel input events for the form though.
+
+See_also: man form_driver
+*/
+int form_driver(FORM* form, int c);
+//driver requests
+enum :int
+{
+  ///move to the given page
+  REQ_NEXT_PAGE         = 0x200,
+  ///ditto
+  REQ_PREV_PAGE         = 0x201,
+  ///ditto
+  REQ_FIRST_PAGE        = 0x202,
+  ///ditto
+  REQ_LAST_PAGE         = 0x203,
+
+  ///move to the specified field
+  REQ_NEXT_FIELD        = 0x204,
+  ///ditto
+  REQ_PREV_FIELD        = 0x205,
+  ///ditto
+  REQ_FIRST_FIELD       = 0x206,
+  ///ditto
+  REQ_LAST_FIELD        = 0x207,
+  ///ditto
+  REQ_SNEXT_FIELD       = 0x208,
+  ///ditto
+  REQ_SPREV_FIELD       = 0x209,
+  ///ditto
+  REQ_SFIRST_FIELD      = 0x20A,
+  ///ditto
+  REQ_SLAST_FIELD       = 0x20B,
+  ///ditto
+  REQ_LEFT_FIELD        = 0x20C,
+  ///ditto
+  REQ_RIGHT_FIELD       = 0x20D,
+  ///ditto
+  REQ_UP_FIELD          = 0x20E,
+  ///ditto
+  REQ_DOWN_FIELD        = 0x20F,
+
+  ///move to the specified place
+  REQ_NEXT_CHAR         = 0x210,
+  ///ditto
+  REQ_PREV_CHAR         = 0x211,
+  ///ditto
+  REQ_NEXT_LINE         = 0x212,
+  ///ditto
+  REQ_PREV_LINE         = 0x213,
+  ///ditto
+  REQ_NEXT_WORD         = 0x214,
+  ///ditto
+  REQ_PREV_WORD         = 0x215,
+  ///ditto
+  REQ_BEG_FIELD         = 0x216,
+  ///ditto
+  REQ_END_FIELD         = 0x217,
+  ///ditto
+  REQ_BEG_LINE          = 0x218,
+  ///ditto
+  REQ_END_LINE          = 0x219,
+  ///ditto
+  REQ_LEFT_CHAR         = 0x21A,
+  ///ditto
+  REQ_RIGHT_CHAR        = 0x21B,
+  ///ditto
+  REQ_UP_CHAR           = 0x21C,
+  ///ditto
+  REQ_DOWN_CHAR         = 0x21D,
+
+  ///insert/overlay a new line
+  REQ_NEW_LINE          = 0x21E,
+  ///insert a blank char/line at the cursor
+  REQ_INS_CHAR          = 0x21F,
+  ///ditto
+  REQ_INS_LINE          = 0x220,
+  ///delete the specified item
+  REQ_DEL_CHAR          = 0x221,
+  ///ditto
+  REQ_DEL_PREV          = 0x222,
+  ///ditto
+  REQ_DEL_LINE          = 0x223,
+  ///ditto
+  REQ_DEL_WORD          = 0x224,
+  ///clear as specified
+  REQ_CLR_EOL           = 0x225,
+  ///ditto
+  REQ_CLR_EOF           = 0x226,
+  ///ditto
+  REQ_CLR_FIELD         = 0x227,
+  ///enter the specified mode
+  REQ_OVL_MODE          = 0x228,
+  ///ditto
+  REQ_INS_MODE          = 0x229,
+  ///scroll the given amount
+  REQ_SCR_FLINE         = 0x22A,
+  ///ditto
+  REQ_SCR_BLINE         = 0x22B,
+  ///ditto
+  REQ_SCR_FPAGE         = 0x22C,
+  ///ditto
+  REQ_SCR_BPAGE         = 0x22D,
+  ///ditto
+  REQ_SCR_FHPAGE        = 0x22E,
+  ///ditto
+  REQ_SCR_BHPAGE        = 0x22F,
+  ///ditto
+  REQ_SCR_FCHAR         = 0x230,
+  ///ditto
+  REQ_SCR_BCHAR         = 0x231,
+  ///ditto
+  REQ_SCR_HFLINE        = 0x232,
+  ///ditto
+  REQ_SCR_HBLINE        = 0x233,
+  ///ditto
+  REQ_SCR_HFHALF        = 0x234,
+  ///ditto
+  REQ_SCR_HBHALF        = 0x235,
+
+  ///validate a field
+  REQ_VALIDATION        = 0x236,
+  ///display next/previous choice
+  REQ_NEXT_CHOICE       = 0x237,
+  ///ditto
+  REQ_PREV_CHOICE       = 0x238,
+
+  MIN_FORM_COMMAND      = 0x200,
+  MAX_FORM_COMMAND      = 0x238
+}
+
 FIELD* new_field(int height, int width,
                 int toprow, int leftcol,
                 int offscreen, int nbuffers);
@@ -57,7 +205,6 @@ FORM* new_form(FIELD** fields);
 int free_form(FORM* form);
 int post_form(FORM* form);
 int unpost_form(FORM* form);
-int form_driver(FORM* form, int c);
 int set_field_fore(FIELD* field, chtype attr);
 chtype field_fore(FIELD* field);
 int set_field_back(FIELD* field, chtype attr);
@@ -116,73 +263,6 @@ int set_fieldtype_choice(
 FIELDTYPE* link_fieldtype(FIELDTYPE* type1,
                          FIELDTYPE* type2);
 
-enum
-{
-  REQ_NEXT_PAGE         = 0x200,
-  REQ_PREV_PAGE         = 0x201,
-  REQ_FIRST_PAGE        = 0x202,
-  REQ_LAST_PAGE         = 0x203,
-
-  REQ_NEXT_FIELD        = 0x204,
-  REQ_PREV_FIELD        = 0x205,
-  REQ_FIRST_FIELD       = 0x206,
-  REQ_LAST_FIELD        = 0x207,
-  REQ_SNEXT_FIELD       = 0x208,
-  REQ_SPREV_FIELD       = 0x209,
-  REQ_SFIRST_FIELD      = 0x20A,
-  REQ_SLAST_FIELD       = 0x20B,
-  REQ_LEFT_FIELD        = 0x20C,
-  REQ_RIGHT_FIELD       = 0x20D,
-  REQ_UP_FIELD          = 0x20E,
-  REQ_DOWN_FIELD        = 0x20F,
-
-  REQ_NEXT_CHAR         = 0x210,
-  REQ_PREV_CHAR         = 0x211,
-  REQ_NEXT_LINE         = 0x212,
-  REQ_PREV_LINE         = 0x213,
-  REQ_NEXT_WORD         = 0x214,
-  REQ_PREV_WORD         = 0x215,
-  REQ_BEG_FIELD         = 0x216,
-  REQ_END_FIELD         = 0x217,
-  REQ_BEG_LINE          = 0x218,
-  REQ_END_LINE          = 0x219,
-  REQ_LEFT_CHAR         = 0x21A,
-  REQ_RIGHT_CHAR        = 0x21B,
-  REQ_UP_CHAR           = 0x21C,
-  REQ_DOWN_CHAR         = 0x21D,
-
-  REQ_NEW_LINE          = 0x21E,
-  REQ_INS_CHAR          = 0x21F,
-  REQ_INS_LINE          = 0x220,
-  REQ_DEL_CHAR          = 0x221,
-  REQ_DEL_PREV          = 0x222,
-  REQ_DEL_LINE          = 0x223,
-  REQ_DEL_WORD          = 0x224,
-  REQ_CLR_EOL           = 0x225,
-  REQ_CLR_EOF           = 0x226,
-  REQ_CLR_FIELD         = 0x227,
-  REQ_OVL_MODE          = 0x228,
-  REQ_INS_MODE          = 0x229,
-  REQ_SCR_FLINE         = 0x22A,
-  REQ_SCR_BLINE         = 0x22B,
-  REQ_SCR_FPAGE         = 0x22C,
-  REQ_SCR_BPAGE         = 0x22D,
-  REQ_SCR_FHPAGE        = 0x22E,
-  REQ_SCR_BHPAGE        = 0x22F,
-  REQ_SCR_FCHAR         = 0x230,
-  REQ_SCR_BCHAR         = 0x231,
-  REQ_SCR_HFLINE        = 0x232,
-  REQ_SCR_HBLINE        = 0x233,
-  REQ_SCR_HFHALF        = 0x234,
-  REQ_SCR_HBHALF        = 0x235,
-
-  REQ_VALIDATION        = 0x236,
-  REQ_NEXT_CHOICE       = 0x237,
-  REQ_PREV_CHOICE       = 0x238,
-
-  MIN_FORM_COMMAND      = 0x200,
-  MAX_FORM_COMMAND      = 0x238
-}
 enum
 {
   NO_JUSTIFICATION = 0,

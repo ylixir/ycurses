a2b08d542c54a99359dc3618967f2ab3b716cfbe
Jude Young <10equals2@gmail.com>
2011-10-30 15:58:34 -0500

    fixed to work with D2.  kinda hackish, warrents further inspection

--- examples/tutorial/menus/menu_simple.d
+++ examples/tutorial/menus/menu_simple.d
@@ -2,56 +2,54 @@ import menu;
 
 const int CTRLD = 4;
 
-char[][] choices = [
-                        "Choice 1",
-                        "Choice 2",
-                        "Choice 3",
-                        "Choice 4",
-                        "Exit"
-                  ];
+immutable char[][] choices = [  "Choice 1",
+                                "Choice 2",
+                                "Choice 3",
+                                "Choice 4",
+                                "Exit"  ];
 
 int main()
-{	ITEM*[] my_items;
-	int c;				
-	MENU* my_menu;
-	int n_choices, i;
-	ITEM* cur_item;
-	
-	
-	initscr();
-	cbreak();
-	noecho();
-	keypad(stdscr, true);
-	
-	n_choices = choices.length;
-	my_items.length = n_choices + 1;
+{   ITEM*[] my_items;
+    int c;
+    MENU* my_menu;
+    int n_choices, i;
+    ITEM* cur_item;
 
-	for(i = 0; i < n_choices; ++i)
-	        my_items[i] = new_item((choices[i]~'\0').ptr, (choices[i]~'\0').ptr);
-	my_items[n_choices] = null;
 
-	my_menu = new_menu(my_items.ptr);
-	mvprintw(LINES - 2, 0, "F1 to Exit");
-	post_menu(my_menu);
-	refresh();
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, true);
 
-	while((c = getch()) != KEY_F(1))
-	{   switch(c)
-	    {	case KEY_DOWN:
-		        menu_driver(my_menu, REQ_DOWN_ITEM);
-				break;
-			case KEY_UP:
-				menu_driver(my_menu, REQ_UP_ITEM);
-				break;
+    n_choices = choices.length;
+    my_items.length = n_choices + 1;
+
+    for(i = 0; i < n_choices; ++i)
+            my_items[i] = new_item((choices[i]~'\0').ptr, (choices[i]~'\0').ptr);
+    my_items[n_choices] = null;
+
+    my_menu = new_menu(my_items.ptr);
+    mvprintw(LINES - 2, 0, "F1 to Exit");
+    post_menu(my_menu);
+    refresh();
+
+    while((c = getch()) != KEY_F(1))
+    {   switch(c)
+        {   case KEY_DOWN:
+                menu_driver(my_menu, REQ_DOWN_ITEM);
+                break;
+            case KEY_UP:
+                menu_driver(my_menu, REQ_UP_ITEM);
+                break;
                         default:
                                 break;
-		}
-	}	
+        }
+    }
 
-	free_item(my_items[0]);
-	free_item(my_items[1]);
-	free_menu(my_menu);
-	endwin();
+    free_item(my_items[0]);
+    free_item(my_items[1]);
+    free_menu(my_menu);
+    endwin();
         return 0;
 }
-	
+
